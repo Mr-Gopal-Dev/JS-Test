4c627079f8fd26531610049e4b9aea5351a888ea
Mr.Gopal <gopal.thecoder@gmail.com>
2025-12-16 18:45:19 +0530

    16/12/2025 - 01

--- vite-project/vite-project/src/main.jsx
+++ vite-project/vite-project/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-//Main - 15/12/2025 - 04
+//Main - 16/12/2025 - 01
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
